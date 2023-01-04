67b6e582a8732788d82a32e3b26330694e1172ea
Hendi <hendiharyadi@outlook.com>
2023-01-04 13:01:18 +0700

    update preauthorize

--- ServerKelompok5/src/main/java/com/mcc72/ServerKelompok5/controllers/EmployeeController.java
+++ ServerKelompok5/src/main/java/com/mcc72/ServerKelompok5/controllers/EmployeeController.java
@@ -14,28 +14,31 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("employee")
-// @PreAuthorize("hasRole('ROLE_ADMIN')")
+@PreAuthorize("hasAnyRole('ROLE_MANAGER', 'ROLE_ADMIN')")
 public class EmployeeController {
 
     private EmployeeService employeeService;
     private UserEntityService userEntityService;
     private StockLeaveService stockLeaveService;
     
-//    @PreAuthorize("hasAuthority('READ_ADMIN')")
+    @PreAuthorize("hasAuthority('READ_ADMIN')")
     @GetMapping
     public List<Map<String, Object>> getAllMap() {
         return employeeService.getAllMap();
     }
-
+    
+    @PreAuthorize("hasAuthority('READ_ADMIN')")
     @GetMapping("/dashboard")
     public Map<String, Object> getEmployee(){
         return employeeService.getEmployee();
     }
     
+    @PreAuthorize("hasAuthority('CREATE_ADMIN')")
     @PostMapping
     public Employee insert(@RequestBody UserRegistrationDto employee) {
         Employee e = employeeService.insert(employee);
@@ -59,24 +62,25 @@ public class EmployeeController {
     public List<Employee> findStaff(){
         return employeeService.findMyStaff();
     }
-
+    
     @GetMapping("/manager/list-permission-staff")
     public List<Permission> findMyStaffPermission(){
         return employeeService.findMyStaffPermission();
     }
     
+    @PreAuthorize("hasAuthority('READ_ADMIN')")
     @GetMapping("/{id}")
     public Object getById(@PathVariable int id) {
         return employeeService.findById(id);
     }
     
-//    @PreAuthorize("hasAuthority('UPDATE_ADMIN')")
+    @PreAuthorize("hasAuthority('UPDATE_ADMIN')")
     @PutMapping("/{id}")
     public Employee update(@PathVariable Integer id, @RequestBody UserRegistrationDto employee) {
         return employeeService.update(id, employee);
     }
     
-//    @PreAuthorize("hasAuthority('DELETE_ADMIN')")
+    @PreAuthorize("hasAuthority('DELETE_ADMIN')")
     @DeleteMapping("/{id}")
     public String delete(@PathVariable Integer id) {
         return employeeService.deleteById(id);
--- ServerKelompok5/src/main/java/com/mcc72/ServerKelompok5/controllers/ProjectController.java
+++ ServerKelompok5/src/main/java/com/mcc72/ServerKelompok5/controllers/ProjectController.java
@@ -9,23 +9,28 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 @RestController
 @RequestMapping("/project")
 @AllArgsConstructor
+@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
 public class ProjectController {
     private ProjectService projectService;
-
+    
+    @PreAuthorize("hasAnyAuthority('READ_ADMIN', 'READ_MANAGER')")
     @GetMapping
     public List<Project> getAllProject(){
         return projectService.getAllProject();
     }
     
+    @PreAuthorize("hasAuthority('READ_ADMIN')")
     @GetMapping("/manager")
     public Object getAll(){
         return projectService.getAll();
     }
-
+    
+    @PreAuthorize("hasAnyAuthority('READ_ADMIN', 'READ_MANAGER')")
     @GetMapping("/{id}")
     public Project getById(@PathVariable int id) {
         return projectService.getById(id);
@@ -35,19 +40,20 @@ public class ProjectController {
     public List<Employee> getMembers(@PathVariable int id){
         return projectService.getMemberProject(id);
     }
-
-
-
+    
+    @PreAuthorize("hasAuthority('CREATE_MANAGER')")
     @PostMapping
     public Project create(@RequestBody ProjectDto projectDto){
         return projectService.create(projectDto);
     }
-
+    
+    @PreAuthorize("hasAuthority('UPDATE_MANAGER')")
     @PutMapping("/{id}")
     public Project update(@PathVariable int id, @RequestBody ProjectDto projectDto){
         return projectService.update(id, projectDto);
     }
-
+    
+    @PreAuthorize("hasAuthority('DELETE_ADMIN')")
     @DeleteMapping("/{id}")
     public Project deleteProject(@PathVariable int id){
         return projectService.deleteProjectById(id);
